7e15aea4901c015fa59ce5e72cc7fda95be23f1f
SimJoonTTMD <wesley0940@gmail.com>
2020-11-14 15:16:59 +0900

    page_plz_last

--- src/App.js
+++ src/App.js
@@ -6,26 +6,84 @@ import 'fullpage-react-fs/dist/index.css'
 function App() {
   return (
     <FullPageContainer showIndicators={true}>
+      
       <FullPagePanel>
         <div className="font">
-           <div id = "title">스마트 마스크 거치대</div>
-          <div >웰시와 괴짜들 - 선린인터넷고등학교 </div>
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/1.png"></img>
+        </div>
+        
+      </FullPagePanel>
+      <FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/2.png"></img>
+        </div>
+        
+      </FullPagePanel>
+      <FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/3.png"></img>
+        </div>
+        
+      </FullPagePanel>
+      <FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/4.png"></img>
+        </div>
+        
+      </FullPagePanel>
+      <FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/5.png"></img>
+        </div>
+        
+      </FullPagePanel><FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/6.png"></img>
+        </div>
+        
+      </FullPagePanel><FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/7.png"></img>
+        </div>
+        
+      </FullPagePanel><FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/8.png"></img>
+        </div>
+        
+      </FullPagePanel><FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/9.png"></img>
+        </div>
+        
+      </FullPagePanel><FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/10.png"></img>
+        </div>
+        
+      </FullPagePanel><FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/11.png"></img>
+        </div>
+        
+      </FullPagePanel><FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/12.png"></img>
+        </div>
+        
+      </FullPagePanel><FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/13.png"></img>
+        </div>
+        
+      </FullPagePanel><FullPagePanel>
+        <div className="font">
+          <img className="pt_img" src="https://sunrinmaker.s3.ap-northeast-2.amazonaws.com/page/page/14.png"></img>
+        </div>
+        
+      </FullPagePanel>
 
-        </div>
-        
-      </FullPagePanel>
-      <FullPagePanel>
-        <div className="font">
-        
-        </div>
-        
-      </FullPagePanel>
-      <FullPagePanel>
-        <div className="font">
-          반기문띠~
-        </div>
-
-      </FullPagePanel>
+     
       
     </FullPageContainer>
   );
