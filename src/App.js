a2bae65a169f9c40c2e704648d6f8e28e29b43de
SimJoonTTMD <wesley0940@gmail.com>
2020-11-14 12:58:14 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,24 @@ import 'fullpage-react-fs/dist/index.css'
 function App() {
   return (
     <FullPageContainer showIndicators={true}>
-      <FullPagePanel bgColor =  'azure'>
-      풀페이지 테스트 1
-      </FullPagePanel>
-      <FullPagePanel bgColor =  'lightgoldenrodyellow'>
-        풀페이지 테스트2
+      <FullPagePanel>
+        <div className="font">
+           <div id = "title">스마트 마스크 거치대</div>
+          <div >웰시와 괴짜들 - 선린인터넷고등학교 </div>
 
+        </div>
+        
       </FullPagePanel>
-      <FullPagePanel bgColor =  'azure'>
-        <h1>테스트3</h1>
+      <FullPagePanel>
+        <div className="font">
+        
+        </div>
+        
+      </FullPagePanel>
+      <FullPagePanel>
+        <div className="font">
+          반기문띠~
+        </div>
 
       </FullPagePanel>
       
